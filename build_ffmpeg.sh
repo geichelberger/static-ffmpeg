768a9e0757c00a2c351f6d07ccde3aaa200da4bd
darolfes <darolfes@users.noreply.github.com>
2021-11-09 13:23:38 +0100

    test

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -335,7 +335,7 @@ compile_rav1e()
 }
 
 # get cpu count
-CPU_CORES="1"
+CPU_CORES="$(grep ^processor /proc/cpuinfo | wc -l)"
 echo "Building with ${CPU_CORES} parallel jobs"
 
 # set path vars
@@ -391,8 +391,8 @@ CXXFLAGS="$CXXFLAGS -static-libgcc -static-libstdc++" \
 compile_with_cmake_sp  libx265 build/linux ../../source \
                        -DENABLE_SHARED:bool=off
 
-compile_with_cmake_sp  libaom-av1 build .. \
-                       -DBUILD_SHARED_LIBS=0
+#compile_with_cmake_sp  libaom-av1 build .. \
+#                       -DBUILD_SHARED_LIBS=0
 
 compile_svtav1         svt_av1
 
@@ -496,7 +496,6 @@ compile_with_configure ffmpeg \
                        --enable-fontconfig \
                        --enable-libopenjpeg \
                        --enable-libspeex \
-                       --enable-libaom \
                        --enable-libsvtav1 \
                        --enable-librav1e \
                        --enable-network \
